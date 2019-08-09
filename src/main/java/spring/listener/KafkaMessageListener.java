3f8d33834b79498353d4038795024ab7c094d95a
chenyihe <15215260433@163.com>
2019-08-09 15:14:12 +0800

    注解里面的参数必须是常量

--- src/main/java/spring/listener/KafkaMessageListener.java
+++ src/main/java/spring/listener/KafkaMessageListener.java
@@ -7,9 +7,12 @@ import org.springframework.kafka.annotation.KafkaListener;
 public class KafkaMessageListener {
 
     private Logger logger = LoggerFactory.getLogger(KafkaMessageListener.class);
+    private static final String topic="kafkatopic_1";
+    private static final String group="test_group_id_01";
 
+    private static final String test="test";
 
-    @KafkaListener(topics="test02", group="test_group_id_01")
+    @KafkaListener(topics=topic, group=group)
     public void listen(String message){
         logger.info("接受消息：{}",message);
     }
