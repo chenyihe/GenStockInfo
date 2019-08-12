e44712f19af83a1aaf7b0737572cc42fb80622fc
chenyihe <15215260433@163.com>
2019-08-12 10:07:45 +0800

    Update KafkaMessageListener.java

--- src/main/java/spring/listener/KafkaMessageListener.java
+++ src/main/java/spring/listener/KafkaMessageListener.java
@@ -3,7 +3,10 @@ package spring.listener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
-
+/**
+*consumer监听类,可以监听多个topic
+*KafkaListener具体用法：https://www.jianshu.com/p/a64defb44a23   
+*/
 public class KafkaMessageListener {
 
     private Logger logger = LoggerFactory.getLogger(KafkaMessageListener.class);
